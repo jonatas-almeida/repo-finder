e7dd1383c9ac1444854b647d54953a504499264b
Jonatas Almeida <jonatas.b.almeida98@gmail.com>
2021-01-09 00:37:36 -0300

    Ranking
    Listagem dos 5 usuários em alta no GitHub

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,9 +8,16 @@ import { Observable } from 'rxjs';
 export class UserService {
 
   baseUrl = "https://api.github.com/"
+  query = "?q=followers%3A%3E%3D1000&ref=searchresults&s=followers&type=Users"
 
   constructor(private http: HttpClient) { }
 
+  //Pega todos os usuários que estão em alta no GitHub
+  getAllUsers():Observable<any>{
+    return this.http.get<any>(`${this.baseUrl}search/users${this.query}`)
+  }
+
+  //Busca o usuário pelo nome de usuário do GitHub
   getUsersByName(userName: string):Observable<any>{
     return this.http.get<any>(`${this.baseUrl}users/${userName}`);
 
